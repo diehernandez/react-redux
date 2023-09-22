f15ca25a9584116c01a3c39927a8e0bd19efaa9e
diehernandez <42649149+diehernandez@users.noreply.github.com>
2023-09-22 18:18:41 +0000

    style: add classnames

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,5 +1,24 @@
+import Button from "./Button";
+
 function App() {
-    return <div>App</div>
+
+    return <div>
+        <div>
+            <Button success >Click here</Button>
+        </div>
+        <div>
+            <Button success >Buy now !</Button>
+        </div>
+        <div>
+            <Button success >See deal!!</Button>
+        </div>
+        <div>
+            <Button success >Hide ads!!</Button>
+        </div>
+        <div>
+            <Button success >Something!!</Button>
+        </div>
+    </div>
 }
 
 export default App;
